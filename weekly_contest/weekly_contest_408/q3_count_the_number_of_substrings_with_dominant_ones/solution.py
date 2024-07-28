4f115005a3d2415b9cca55bd834e731733b4e2f2
along <wuhl6@mail2.sysu.edu.cn>
2024-07-28 14:08:15 +0800

    perf: improve weekly contest 408, q3 (Still Time Limit Exceeded)
    835 / 881 testcases passed

--- weekly_contest/weekly_contest_408/q3_count_the_number_of_substrings_with_dominant_ones/solution.py
+++ weekly_contest/weekly_contest_408/q3_count_the_number_of_substrings_with_dominant_ones/solution.py
@@ -3,29 +3,30 @@ from typing import List
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
         s_len = len(s)
-        nums_of_one = numbers_of_one(s, s_len)
-        total = 0
         # 注意右边是开区间，即 [i, j)
+        totals_of_one = get_totals_of_one(s, s_len)
+        num = 0
         for i in range(s_len):
-            for j in range(i+1, s_len+1):
-                num_of_one = nums_of_one[i][j]
-                num_of_zero = j - i - num_of_one
+            for j in range(i, s_len):
+                num_of_one = totals_of_one[j+1] - totals_of_one[i]
+                num_of_zero = j - i + 1 - num_of_one
                 if num_of_one >= num_of_zero ** 2:
-                    total += 1
-        return total
+                    num += 1
 
-def numbers_of_one(s: str, s_len: int) -> List[List[int]]:
-    result = [[0 for _ in range(s_len+1)] for _ in range(s_len)]
+        return num
+
+def get_totals_of_one(s: str, s_len: int):
+    totals = [0 for _ in range(s_len + 1)]
     for i in range(s_len):
-        for j in range(i+1, s_len+1):
-            if s[j-1] == '1':
-                result[i][j] = result[i][j-1] + 1
-            else:
-                result[i][j] = result[i][j-1]
-
-    return result
+        if s[i] == '1':
+            totals[i+1] = totals[i] + 1
+        else:
+            totals[i+1] = totals[i]
+    return totals
 
 if __name__ == "__main__":
     solution = Solution()
-    assert solution.numberOfSubstrings("00011") == 5
-    assert solution.numberOfSubstrings("101101") == 16
+    s = '00011'
+    assert solution.numberOfSubstrings(s) == 5
+    s = '101101'
+    assert solution.numberOfSubstrings(s) == 16
